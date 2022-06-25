0243657063d1c1dfc5eec21121dc395025ad7e11
Christian Aquino <cjfaquino@gmail.com>
2022-06-25 04:00:12 -0700

    add function to validate pw

--- scripts.js
+++ scripts.js
@@ -1,6 +1,24 @@
-const pass = document.getElementById('password');
-const passCheck = document.getElementById('pw_check');
+const pass = document.getElementById("password");
+const passCheck = document.getElementById("pw_check");
+const passNoValid = document.querySelector(".requirements");
 
-passCheck.addEventListener('change', addValidation)
-pass.addEventListener('change', addValidation)
+passCheck.addEventListener("change", setValidation);
+pass.addEventListener("change", setValidation);
 
+function setValid() {
+    passNoValid.style.visibility = "hidden";
+    pass.style.border = "1px solid grey";
+    passCheck.style.border = "1px solid grey";
+}
+
+function setInvalid() {
+    passNoValid.style.visibility = "visible";
+    pass.style.border = "1px solid red";
+    passCheck.style.border = "1px solid red";
+}
+
+function setValidation() {
+    (pass.value !== passCheck.value && passCheck.value) ?
+    setInvalid() :
+    setValid()
+}
