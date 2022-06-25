b9c74067fa3db8656dd0bae12f2a5058cee1a779
Christian Aquino <cjfaquino@gmail.com>
2022-06-25 03:24:37 -0700

    add event listeners

---
+++ scripts.js
@@ -0,0 +1,6 @@
+const pass = document.getElementById('password');
+const passCheck = document.getElementById('pw_check');
+
+passCheck.addEventListener('change', addValidation)
+pass.addEventListener('change', addValidation)
+
