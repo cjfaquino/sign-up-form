9724d2f945912959f689dea360a750de30481ff2
Christian Aquino <cjfaquino@gmail.com>
2022-10-14 23:38:17 -0700

    add password pattern

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import './styles.css';
 import '../components/formatPhone';
 import '../components/checkValid';
+
+const password = document.querySelector('#password');
+
+password.pattern =
+  '^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$';
