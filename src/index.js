1a6fb243cafbcc0cb299f830d710ae065b600fba
Christian Aquino <cjfaquino@gmail.com>
2022-10-14 23:36:32 -0700

    remove old form validation

--- src/index.js
+++ src/index.js
@@ -1,35 +1,3 @@
 import './styles.css';
 import '../components/formatPhone';
-
-const pass = document.getElementById('password');
-const passCheck = document.getElementById('pw_check');
-const passNoValid = document.querySelector('.requirements');
-
-function setValid() {
-  passNoValid.style.visibility = 'hidden';
-  pass.style.border = '1px solid grey';
-  passCheck.style.border = '1px solid grey';
-}
-
-function setInvalid() {
-  passNoValid.style.visibility = 'visible';
-  pass.style.border = '1px solid red';
-  passCheck.style.border = '1px solid red';
-}
-
-function setValidation() {
-  if (pass.value !== passCheck.value && passCheck.value) {
-    setInvalid();
-  } else setValid();
-}
-
-function demo() {
-  pass.value = 'testdemocase';
-  passCheck.value = 'demo';
-  setValidation();
-}
-
-passCheck.addEventListener('change', setValidation);
-pass.addEventListener('change', setValidation);
-
-demo();
+import '../components/checkValid';
