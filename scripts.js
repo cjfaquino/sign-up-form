1ab6d24552d2a5ff4e3d4c5862a4be4f0c41212f
Christian Aquino <cjfaquino@gmail.com>
2022-10-14 22:28:14 -0700

    format document

--- scripts.js
+++ scripts.js
@@ -1,32 +1,30 @@
-const pass = document.getElementById("password");
-const passCheck = document.getElementById("pw_check");
-const passNoValid = document.querySelector(".requirements");
+const pass = document.getElementById('password');
+const passCheck = document.getElementById('pw_check');
+const passNoValid = document.querySelector('.requirements');
 
-passCheck.addEventListener("change", setValidation);
-pass.addEventListener("change", setValidation);
+passCheck.addEventListener('change', setValidation);
+pass.addEventListener('change', setValidation);
 
 function setValid() {
-    passNoValid.style.visibility = "hidden";
-    pass.style.border = "1px solid grey";
-    passCheck.style.border = "1px solid grey";
+  passNoValid.style.visibility = 'hidden';
+  pass.style.border = '1px solid grey';
+  passCheck.style.border = '1px solid grey';
 }
 
 function setInvalid() {
-    passNoValid.style.visibility = "visible";
-    pass.style.border = "1px solid red";
-    passCheck.style.border = "1px solid red";
+  passNoValid.style.visibility = 'visible';
+  pass.style.border = '1px solid red';
+  passCheck.style.border = '1px solid red';
 }
 
 function setValidation() {
-    (pass.value !== passCheck.value && passCheck.value) ?
-    setInvalid() :
-    setValid()
+  pass.value !== passCheck.value && passCheck.value ? setInvalid() : setValid();
 }
 
 function demo() {
-    pass.value = "testdemocase"
-    passCheck.value = "demo"
-    setValidation()
+  pass.value = 'testdemocase';
+  passCheck.value = 'demo';
+  setValidation();
 }
 
 demo();
