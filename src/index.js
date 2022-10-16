deabb3f503b82d95be70100db6a066650769f2c1
Christian Aquino <cjfaquino@gmail.com>
2022-10-15 18:41:16 -0700

    add first & last name pattern

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import '../components/formatPhone';
 import '../components/checkValid';
 
 const password = document.querySelector('#password');
+const firstN = document.querySelector('#firstName');
+const lastN = document.querySelector('#lastName');
+
+firstN.pattern = "/^[a-z ,.'-]+$/i";
+lastN.pattern = "/^[a-z ,.'-]+$/i";
 
 password.pattern =
   '^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$';
