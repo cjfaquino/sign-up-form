d8b53a3679bb70eaea338fc922118d1a9e68d1cb
Christian Aquino <cjfaquino@gmail.com>
2022-10-15 19:37:34 -0700

    add copyplugin for imgs/

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -7,6 +8,11 @@ module.exports = {
   devServer: {
     static: './dist',
   },
+  plugins: [
+    new CopyPlugin({
+      patterns: [{ from: 'src/imgs/', to: 'imgs' }],
+    }),
+  ],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
